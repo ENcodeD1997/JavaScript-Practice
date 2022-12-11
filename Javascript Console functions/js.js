ab5c7c5787d59cda20beb087101172628fcf9c6e
Javonte Bolton <boltonjavonte@gmail.com>
2022-12-10 20:56:16 -0600

    Implemented if else statements into console

---
+++ Javascript Console functions/js.js
@@ -0,0 +1,59 @@
+const original = "the weather is warm"
+const newString = original.replace ("warm", "cold")
+console.log(newString)
+
+let a = 5
+const javaScript = "Javascript is so very easy it is insane"
+const replacement = javaScript.replace ("easy", "hard")
+let b = 5
+console.log(replacement)
+console.log (a + b)
+
+const myName = "Javonte"
+const Greeting = `Hi I am ${myName}`
+console.log(Greeting) 
+
+const c = 5
+const d = 20
+const e = 120
+const f = 500
+const g = 155
+console.log(c + d / e * f / g)
+
+let hour = 1;
+
+if (hour >= 3 && hour < 12) {
+    console.log('Good Evening');
+}
+else if ( hour <= 1) {
+    console.log('Good Morning');
+}
+
+let z = 20
+let y = 5
+const answer = y + z
+console.log(answer)
+if (answer >= 26) {
+    console.log('false')
+} 
+else (answer <= 25); {
+    console.log('true')
+} 
+
+let i = 10
+let l = 20
+
+ if( i > l ) {
+    console.log(true);
+ }
+ else {
+    console.log(false);
+ }
+
+ let month = 10
+ if (12>=10) {
+    console.log(true);
+ }
+ else {
+    console.log (false);
+ }
