7d8b1a507abd7381623f2a259ae71797604d9320
Javonte Bolton <boltonjavonte@gmail.com>
2023-01-03 19:38:56 -0600

    Using THE DOM to manipulate text

---
+++ JavaScript & Dom manipulation/THE DOM.js
@@ -0,0 +1,9 @@
+const paragraph = document.getElementById("pp");
+const  text = document.createTextNode('I\'m red!');
+paragraph.style.color = 'red';
+paragraph.appendChild(text);
+
+const next = document.getElementById("ppp");
+const wext = document.createTextNode("This is more of the test lets see if it pops up");
+next.style.color = ('red');
+next.appendChild(wext);
